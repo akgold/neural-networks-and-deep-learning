47ebf37dea972226483f2ff2006d082d7b223cf9
Alex K Gold <alexkgold@gmail.com>
2017-06-02 13:02:16 -0400

    Add working version. Does the math work?

--- network_1.R
+++ network_1.R
@@ -27,21 +27,30 @@ init_network <- function(sizes) {
 
 #' Format Input and Output Data for Neural Network
 #'
+#' Note that the output data, y, should be a data frame with the same number 
+#' of columns as the network's output layer.
+#'
 #' @param x data frame of xs for neural network
-#' @param y numeric vector of outcomes for network
+#' @param y data frame of ys for neural network
 #'
 #' @return formatted data, of class "nn_data"
 #' @export
 #'
 #' @examples
-#' dat <- nn_make_data(data.frame(x1 = sample(0:1, 10, replace = T),
-#' x2 = sample(0:1, 10, replace = T)), sample(0:1, 10, replace = T))
-nn_make_data <- function(x, y){
+#' nn <- init_network(c(2,4,2))
+#' nn_make_data(nn, data.frame(sample(0:1, 10, replace = T),
+#' sample(0:1, 10, replace = T)), sample(1:2, 10, replace = T))
+nn_make_data <- function(network, x, y){
   # Error Checking
-  stopifnot(is.data.frame(x))
-  stopifnot(is.numeric(y))
-  stopifnot(nrow(x) == length(y))
-
+  stopifnot(is.data.frame(x) & is.numeric(y))
+  stopifnot(nrow(x) == nrow(y))
+  stopifnot(ncol(x) == network$sizes[1])
+  stopifnot(max(y) <= network$sizes[length(network$sizes)])
+  
+  # Turn categorical Y into vector
+  y <- factor(y, levels = seq(network$sizes[length(network$sizes)]))
+  y <- as.data.frame(model.matrix(~ y - 1))
+  
   list(x = x, y = y)
 }
 
@@ -58,36 +67,35 @@ nn_make_data <- function(x, y){
 #' @export
 #'
 #' @examples
-#' nn <- init_network(3,4,4)
-#' training <- nn_make_data(data.frame(x1 = sample(0:1, 10, replace = T),
-#' x2 = sample(0:1, 10, replace = T),
-#' x3 = sample(0:1, 10, replace = T)), sample(1:4, 10, replace = T))
-#' test <- nn_make_data(data.frame(x1 = sample(0:1, 10, replace = T),
-#' x2 = sample(0:1, 10, replace = T),
-#' x3 = sample(0:1, 10, replace = T)), sample(1:4, 10, replace = T))
+#' nn <- init_network(c(3,4,4))
+#' training <- nn_make_data(nn, data.frame(runif(10), runif(10), runif(10)), 
+#' sample(1:4, 10, replace = T))
+#' test <- nn_make_data(nn, data.frame(runif(10), runif(10), runif(10)), 
+#' sample(1:4, 10, replace = T))
 #'
-#' nn <- run_sgd(network = nn,
-#' training_data = training,
-#' epochs = 3, split_size = 1, eta = 3,
-#' test_data = test)
+#' nn <- run_sgd(network = nn, training_data = training, epochs = 3, 
+#' split_size = 1, eta = 3, test_data = test)
 run_sgd <- function(network, training_data, epochs, split_size, eta,
                     test_data = NULL) {
   for (i in seq_len(epochs)) {
     # Randomly assign mini-batches
-    n_folds <- ceiling(nrow(training_data)/split_size)
-    fold <- sample(1:n_folds, training_data$length, replace = F)
+    n_folds <- ceiling(nrow(training_data$x)/split_size)
+    fold <- sample(1:n_folds, nrow(training_data$x), replace = F)
 
-    for (j in seq_len(n_folds)) {
+    for (j in seq_len(n_folds)) {      
+      print(j)
+      print(network)
       # Update network for each mini-batch
       mini_batch <- list()
       mini_batch$x <- training_data$x[fold == j,]
       mini_batch$y <- training_data$y[fold == j,]
       network <- update_batch(network, mini_batch, eta)
 
+
       # Print diagnostics
       if (!is.null(test_data)) {
         cat(sprintf("Epoch %s: %s/%s",
-                    j, nn_eval(test_data), length(test_data)))
+                    j, nn_eval(nn, test_data), nrow(test_data$x)))
       } else {
         sprintf("Epoch %s: ", j)
       }
@@ -109,20 +117,23 @@ run_sgd <- function(network, training_data, epochs, split_size, eta,
 #'
 #' @examples
 #' nn <- init_network(c(2, 4, 3))
-#' mb <- nn_make_data(x = data.frame(1:6, 7:12), y = runif(6))
+#' mb <- nn_make_data(nn, x = data.frame(1:5, 6:10), 
+#'     y = sample(1:3, 10, replace = T))
 #' nn <- update_batch(nn, mb, 3)
+#' nn
 update_batch <- function(network, mini_batch, eta) {
   delt <- backprop(network, mini_batch, eta)
+  n <- nrow(mini_batch$x)
 
-  network$weights <- lapply(network$n_layers,
+  network$weights <- lapply(seq_len(network$n_layers - 1),
                             function(i) {
                               network$weights[[i]] -
-                                (eta/nrow(mini_batch))*delt$weights[[i]]
+                                eta/n*delt$weights[[i]]
                             })
-  network$biases <- lapply(network$n_layers,
+  network$biases <- lapply(seq_len(network$n_layers - 1),
                            function(i) {
                              network$biases[[i]] -
-                               (eta/nrow(mini_batch))*delt$biases[[i]]
+                               eta/n*delt$biases[[i]]
                            })
   network
 }
@@ -136,26 +147,31 @@ update_batch <- function(network, mini_batch, eta) {
 #' @param eta
 #'
 #' @examples
-#' mb <- nn_make_data(data.frame(x1 = sample(0:1, 10, replace = T),
-#' x2 = sample(0:1, 10, replace = T)), sample(0:1, 10, replace = T))
 #' nn <- init_network(c(2, 3, 4))
+#' mb <- nn_make_data(nn, data.frame(runif(10), runif(10)), 
+#' sample(1:4, 10, replace = T))
 #' backprop(nn, mb, 3)
 backprop <- function(network, mini_batch, eta) {
+  n <- nrow(mini_batch$x)
+  
   # Backpropogate all rows of mini-batch
-  bps <- lapply(1:length(mini_batch$y),
+  bps <- lapply(seq(n),
                 function(i) backprop_single(network,
                                             mini_batch$x[i,],
-                                            mini_batch$y[i]))
-  bps <- purrr::transpose(bps)
-
-  # Average across rows to change weights
-  for (layer in 1:network$n_layers){
-    network$weights[[layer]] <- network$weights[[layer]] -
-      eta / length(bps$weights) *
-      purrr::reduce(bps$weights, function(x, y) x[[layer]] + y[[layer]])
-    network$biases[[layer]] <- network$biases[[layer]] -
-      eta / length(bps$biases) *
-      purrr::reduce(bps$biases, function(x, y) x[[layer]] + y[[layer]])
+                                            mini_batch$y[i,]))
+  
+  # Average across runs of backprops to update network
+  for (layer in seq(network$n_layers-1)) {
+    w <- network$weights[[layer]]
+    b <- network$biases[[layer]]
+    
+    # Reduce all delta weights into network
+    for(i in seq(n)){
+      w <- w - eta / n * bps[[i]]$weights[[layer]]
+      b <- b - eta / n * bps[[i]]$biases[[layer]]
+    }
+    network$weights[[layer]] <- w
+    network$biases[[layer]] <- b
   }
 
   network
@@ -170,9 +186,9 @@ backprop <- function(network, mini_batch, eta) {
 #'
 #' @examples
 #' network <- init_network(c(4, 3, 2))
-#' d <- nn_make_data(x = data.frame(runif(10), runif(10), runif(10), 
-#' runif(10)), y = runif(10))
-#' nab <- backprop_single(network, d$x[1,], d$y[1])
+#' d <- nn_make_data(network, x = data.frame(runif(10), runif(10), runif(10), 
+#' runif(10)), y = sample(1:2, 10, replace = T))
+#' nab <- backprop_single(network, d$x[1,], d$y[1,])
 #' nab
 backprop_single <- function(network, x, y){
   layers <- network$n_layers
@@ -196,59 +212,36 @@ backprop_single <- function(network, x, y){
   nabla$biases <- list()
 
   # Get Nablas for output layer
-  nabla$biases[[layers-1]] <- t(cost_derivative(activations[[layers]], y) *
+  nabla$biases[[layers-1]] <- as.numeric(cost_derivative(activations[[layers]], y) *
     sigmoid_prime(z[[layers-1]]))
-  nabla$weights[[layers-1]] <- t(nabla$biases[[layers-1]]) %*%
-    t(activations[[layers-1]])
-
+  # Why the heck do I need t(t(.))??
+  nabla$weights[[layers-1]] <- nabla$biases[[layers-1]] %*% 
+    t(activations[[layers-1]]) 
+    
+  
   # Propogate error through network
-  # The indices took FOREVER
   for (layer in seq(layers - 2, 1)){
     sp <- sigmoid_prime(z[[layer]])
-    nabla$biases[[layer]] <- nabla$biases[[layer + 1]] %*% 
-      network$weights[[layer + 1]] * t(sp)
+    nabla$biases[[layer]] <- as.numeric(nabla$biases[[layer + 1]] %*% 
+      network$weights[[layer + 1]] * t(sp))
     
-    nabla$weights[[layer]] <- t(nabla$biases[[layer]]) %*%
+    nabla$weights[[layer]] <- t(t(nabla$biases[[layer]])) %*%
       activations[[layer]]
   }
-
+  
   nabla
-}
-
-
-forward_pass <- function(network, x,
-                         return = NULL){
-  stopifnot(is.null(return) | 1:network$n_layers)
-
-  z <- list()
-  activations <- list()
-  activations[[1]] <- as.numeric(x)
-
-  for (layer in 1:layers){
-    z[[layer]] = network$weights[[layer]] %*% activations[[layer]]
-    activations[[layer+1]] <- sigmoid(z[[layer]])
-  }
-
-  if (!is.null(return)) {
-
-  }
-}
-
-nn_eval <- function(){
-
 }
 
 #' Partial Derivatives of Output wrt final activations
 #' @param output_activations activations of final layer
-#' @param y actual ys
+#' @param y vector of actual ys
 #'
 #'  @return vector of derivatives
 #'  @examples
-#'  cost_derivative(runif(4), runif(4))
+#'  cost_derivative(runif(4), c(0,0,1,0))
 cost_derivative <- function(output_activations, y){
   output_activations - y
 }
-
 
 #' Compute sigmoid function of argument
 #'
@@ -274,4 +267,62 @@ sigmoid <- function(z) {
 #' sigmoid_prime(runif(5))
 sigmoid_prime <- function(z) {
   sigmoid(z) * (1 - sigmoid(z))
+}
+
+#' Evaluate Performance of network
+#'
+#' @param nn a neural network created by ?init_network
+#' @param test_data test data as created by ?nn_make_data
+#'
+#' @return number of outputs the network gets right
+#'
+#' @examples
+#' nn <- init_network(c(3,4,5))
+#' d <- nn_make_data(nn, data.frame(runif(10), runif(10), runif(10)), 
+#' sample(1:5, 10, replace = T))
+#' nn_eval(nn, d)
+#' 
+nn_eval <- function(nn, test_data){
+  # Get output of network as vector
+  outs <- feedforward(nn, test_data$x)
+  
+  # Get single output as argmax of y
+  out_num <- apply(outs, 1, function(y) which(y == max(y)))
+  # Do same for actual test data
+  y_num <- apply(test_data$y, 1, function(y) which(y == max(y)))
+  
+  sum(out_num == y_num)
+}
+
+
+#' Get output of network for given set of Xs
+#'
+#' @param nn a neural network
+#' @param x a data.frame of Xs
+#' 
+#' @return the output of the output layer of network given X
+#' 
+#' @example 
+#' nn <- init_network(c(3,4,5))
+#' d <- nn_make_data(nn, data.frame(runif(10), runif(10), runif(10)), 
+#' sample(1:5, 10, replace = T))
+#' feedforward(nn, d$x)
+feedforward <- function(nn, x){
+  
+  # Rows of x to list
+  x <- split(x, seq(nrow(x)))
+  
+  # Loop over x vectors
+  o <- lapply(x, function(act){
+    act <- as.numeric(act)
+    # Feed forward each input activation
+    for (layer in seq_len(nn$n_layers-1)){
+      z <- nn$weights[[layer]] %*% act +
+        nn$biases[[layer]]
+      act <- sigmoid(z)
+    }
+    t(act)
+  })
+  
+  as.data.frame(do.call(rbind, o))
 }
